6486e754a9fdbb71c7f58b219deae06e9c186add
Mirko Lenz <mirko@mirkolenz.com>
2022-12-14 23:14:31 +0100

    refactor: add python docs link

--- makejinja/cli.py
+++ makejinja/cli.py
@@ -16,12 +16,15 @@ app = typer.Typer()
 
 def import_file(path: Path) -> ModuleType:
     # https://stackoverflow.com/a/41595552
+    # https://docs.python.org/3.11/library/importlib.html#importing-a-source-file-directly
     name = str(uuid()).lower().replace("-", "")
     spec = importlib.util.spec_from_file_location(name, path)
     assert spec is not None
+
     module = importlib.util.module_from_spec(spec)
     sys.modules[name] = module
     assert spec.loader is not None
+
     spec.loader.exec_module(module)
 
     return module
