cfc13ae4e345330d27c32f92acac2478c542ff6a
Mirko Lenz <mirko@mirkolenz.com>
2023-01-15 16:49:25 +0100

    docs: update cli help messages

--- makejinja/config.py
+++ makejinja/config.py
@@ -19,50 +19,82 @@ from jinja2.defaults import (
 
 @ts.settings(frozen=True)
 class Delimiter:
-    block_start: str = ts.option(default=BLOCK_START_STRING)
+    block_start: str = ts.option(
+        default=BLOCK_START_STRING, help="The string marking the beginning of a block."
+    )
     block_end: str = ts.option(
-        default=BLOCK_END_STRING,
-    )
-    comment_start: str = ts.option(
-        default=COMMENT_START_STRING,
-    )
-    comment_end: str = ts.option(
-        default=COMMENT_END_STRING,
+        default=BLOCK_END_STRING, help="The string marking the end of a block."
     )
     variable_start: str = ts.option(
         default=VARIABLE_START_STRING,
+        help="The string marking the beginning of a print statement.",
     )
     variable_end: str = ts.option(
         default=VARIABLE_END_STRING,
+        help="The string marking the end of a print statement.",
+    )
+    comment_start: str = ts.option(
+        default=COMMENT_START_STRING,
+        help="The string marking the beginning of a comment.",
+    )
+    comment_end: str = ts.option(
+        default=COMMENT_END_STRING, help="The string marking the end of a comment."
     )
 
 
 @ts.settings(frozen=True)
 class Prefix:
     line_statement: t.Optional[str] = ts.option(
-        default=LINE_STATEMENT_PREFIX, help="TODO"
+        default=LINE_STATEMENT_PREFIX,
+        help="If given and a string, this will be used as prefix for line based statements.",
     )
-    line_comment: t.Optional[str] = ts.option(default=LINE_COMMENT_PREFIX, help="TODO")
+    line_comment: t.Optional[str] = ts.option(
+        default=LINE_COMMENT_PREFIX,
+        help="If given and a string, this will be used as prefix for line based comments.",
+    )
 
 
 @ts.settings(frozen=True)
 class Internal:
     optimized: bool = ts.option(
-        default=False,
+        default=True,
         click={"hidden": True},
-        help="TODO",
+        help="Should the [optimizer](https://github.com/Pfern/jinja2/blob/master/jinja2/optimizer.py) be enabled?",
     )
     autoescape: bool = ts.option(
         default=False,
         click={"param_decls": "--internal-autoescape", "hidden": True},
-        help="TODO",
+        help="""
+            If set to `True` the XML/HTML autoescaping feature is enabled by default.
+            For more details about autoescaping see `markupsafe.Markup`.
+        """,
     )
-    cache_size: int = ts.option(default=400, click={"hidden": True}, help="TODO")
-    auto_reload: bool = ts.option(default=True, click={"hidden": True}, help="TODO")
+    cache_size: int = ts.option(
+        default=0,
+        click={"hidden": True},
+        help="""
+            The size of the cache.
+            If the cache size is set to a positive number like `400`,
+            it means that if more than 400 templates are loaded the loader will clean out the least recently used template.
+            If the cache size is set to `0`, templates are recompiled all the time.
+            If the cache size is `-1` the cache will not be cleaned.
+        """,
+    )
+    auto_reload: bool = ts.option(
+        default=False,
+        click={"hidden": True},
+        help="""
+            Some loaders load templates from locations where the template sources may change (ie: file system or database).
+            If `auto_reload` is set to `True`, every time a template is requested, the loader checks if the source changed and if yes,
+            it will reload the template.  For higher performance it's possible to disable that.
+        """,
+    )
     enable_async: bool = ts.option(
         default=False,
         click={"param_decls": "--internal-enable-async", "hidden": True},
-        help="TODO",
+        help="""
+            If set to true this enables async template execution which allows using async functions and generators.
+        """,
     )
 
 
@@ -72,31 +104,32 @@ class Whitespace:
         default=True,
         click={"param_decls": "--trim-blocks/--no-trim-blocks"},
         help="""
-                If an application configures Jinja to trim_blocks, the first newline after a template tag is removed automatically (like in PHP).
-                Refer to the [Jinja docs](https://jinja.palletsprojects.com/en/3.1.x/templates/#whitespace-control) for more details.
-            """,
+            If this is set to `True`, the first newline after a block is removed (block, not variable tag!).
+        """,
     )
     lstrip_blocks: bool = ts.option(
         default=True,
         click={"param_decls": "--lstrip-blocks/--no-lstrip-blocks"},
         help="""
-                The lstrip_blocks option can also be set to strip tabs and spaces from the beginning of a line to the start of a block.
-                (Nothing will be stripped if there are other characters before the start of the block.)
-                Refer to the [Jinja docs](https://jinja.palletsprojects.com/en/3.1.x/templates/#whitespace-control) for more details.
-            """,
+            If this is set to `True`, leading spaces and tabs are stripped from the start of a line to a block.
+        """,
     )
     newline_sequence: str = ts.option(
         default=NEWLINE_SEQUENCE,
         click={"param_decls": "--newline-sequence"},
-        help="TODO Literal['\\n', '\\r\\n', '\\r']",
+        help="""
+            The sequence that starts a newline.
+            Must be one of `\\r`, `\\n` or `\\r\\n`.
+            The default is `\\n` which is a useful default for Linux and OS X systems as well as web applications.
+        """,
     )
     keep_trailing_newline: bool = ts.option(
         default=KEEP_TRAILING_NEWLINE,
         click={"param_decls": "--keep-trailing-newline"},
         help="""
-                By default, Jinja also removes trailing newlines. To keep single trailing newlines, configure Jinja to keep_trailing_newline.
-                Refer to the [Jinja docs](https://jinja.palletsprojects.com/en/3.1.x/templates/#whitespace-control) for more details.
-            """,
+            Preserve the trailing newline when rendering templates.
+            The default is `False`, which causes a single newline, if present, to be stripped from the end of the template.
+        """,
     )
 
 
@@ -105,60 +138,60 @@ class Config:
     input: Path = ts.option(
         click={"type": click.Path(exists=True, file_okay=False, path_type=Path)},
         help="""
-                Path to a folder containing template files.
-                It is passed to Jinja's [FileSystemLoader](https://jinja.palletsprojects.com/en/3.1.x/api/#jinja2.FileSystemLoader) when creating the environment.
-            """,
+            Path to a folder containing template files.
+            It is passed to Jinja's [FileSystemLoader](https://jinja.palletsprojects.com/en/3.1.x/api/#jinja2.FileSystemLoader) when creating the environment.
+        """,
     )
     output: Path = ts.option(
         click={"type": click.Path(file_okay=False, writable=True, path_type=Path)},
         help="""
-                Path to a folder where the rendered templates are stored.
-                makejinja preserves the relative paths in the process, meaning that you can even use it on nested directories.
-            """,
+            Path to a folder where the rendered templates are stored.
+            makejinja preserves the relative paths in the process, meaning that you can even use it on nested directories.
+        """,
     )
     input_pattern: str = ts.option(
         default="**/*",
         help="""
-                Glob pattern to search for files in `input_folder`.
-                Accepts all pattern supported by [`fnmatch`](https://docs.python.org/3/library/fnmatch.html#module-fnmatch).
-                If a file is matched by this pattern and does not end with the specified `jinja-suffix`, it is copied over to the `output_folder`.
-                **Note:** Do not add a special suffix used by your template files here, instead use the `jinja-suffix` option.
-            """,
+            Glob pattern to search for files in `input_folder`.
+            Accepts all pattern supported by [`fnmatch`](https://docs.python.org/3/library/fnmatch.html#module-fnmatch).
+            If a file is matched by this pattern and does not end with the specified `jinja-suffix`, it is copied over to the `output_folder`.
+            **Note:** Do not add a special suffix used by your template files here, instead use the `jinja-suffix` option.
+        """,
     )
     jinja_suffix: str = ts.option(
         default=".jinja",
         help="""
-                File ending of Jinja template files.
-                All files with this suffix in `input_folder` matched by `pattern` are passed to the Jinja renderer.
-                **Note:** Should be provided *with* the leading dot.
-            """,
+            File ending of Jinja template files.
+            All files with this suffix in `input_folder` matched by `pattern` are passed to the Jinja renderer.
+            **Note:** Should be provided *with* the leading dot.
+        """,
     )
     keep_jinja_suffix: bool = ts.option(
         default=False,
         click={"param_decls": "--keep-jinja-suffix"},
         help="""
-                Decide whether the specified `jinja-suffix` is removed from the file after rendering.
-            """,
+            Decide whether the specified `jinja-suffix` is removed from the file after rendering.
+        """,
     )
     copy_tree: bool = ts.option(
         default=False,
         click={"param_decls": "--copy-tree"},
         help="""
-                If your `input_folder` containes additional files besides Jinja templates, you may want to copy them to `output_folder` as well.
-                This operation maintains the metadata of all files and folders, meaning that tools like `rsync` will treat them exactly like the original ones.
-                **Note:** Even if set to `no-copy-tree`, files that are matched by your provided `pattern` within `input_folder` are still copied over.
-                In both cases, a file's metadata is untouched.
-                The main difference is that with `copy-tree`, folders keep their metadata while matched files are copied to newly-created subfolders that differ in their metadata.
-            """,
+            If your `input_folder` containes additional files besides Jinja templates, you may want to copy them to `output_folder` as well.
+            This operation maintains the metadata of all files and folders, meaning that tools like `rsync` will treat them exactly like the original ones.
+            **Note:** Even if set to `no-copy-tree`, files that are matched by your provided `pattern` within `input_folder` are still copied over.
+            In both cases, a file's metadata is untouched.
+            The main difference is that with `copy-tree`, folders keep their metadata while matched files are copied to newly-created subfolders that differ in their metadata.
+        """,
     )
     keep_empty: bool = ts.option(
         default=False,
         click={"param_decls": "--keep-empty"},
         help="""
-                Some Jinja template files may be empty after rendering (e.g., if they only contain macros that are imported by other templates).
-                By default, we do not copy such empty files.
-                If there is a need to have them available anyway, you can adjust that.
-            """,
+            Some Jinja template files may be empty after rendering (e.g., if they only contain macros that are imported by other templates).
+            By default, we do not copy such empty files.
+            If there is a need to have them available anyway, you can adjust that.
+        """,
     )
     data: list[Path] = ts.option(
         factory=list,
@@ -167,7 +200,7 @@ class Config:
             "param_decls": "--data",
         },
         help="""
-                Load variables from yaml/yml or toml files for use in your Jinja templates.
+                Load variables from yaml/yml/toml files for use in your Jinja templates.
                 The defintions are passed to Jinja's `render` function.
                 Can either be a file or a folder containg files.
                 **Note:** This option may be passed multiple times to pass a list of values.
@@ -180,18 +213,19 @@ class Config:
             "param_decls": "--loader",
         },
         help="""
-            Load custom code into the program. TODO: More details
+            Load custom code into the program.
+            TODO: More details
+            **Note:** This option may be passed multiple times to pass a list of values.
         """,
     )
     extensions: list[str] = ts.option(
         factory=list,
         click={"param_decls": "--extension"},
         help="""
-                Extend Jinja's parser by loading the specified extensions.
-                An overview of the built-in ones can be found on the [project website](https://jinja.palletsprojects.com/en/3.1.x/extensions/).
-                Currently, only those built-in filters are allowed.
-                **Note:** This option may be passed multiple times to pass a list of values.
-            """,
+            List of Jinja extensions to use as strings of import paths.
+            An overview of the built-in ones can be found on the [project website](https://jinja.palletsprojects.com/en/3.1.x/extensions/).
+            **Note:** This option may be passed multiple times to pass a list of values.
+        """,
     )
     delimiter: Delimiter = ts.option(factory=Delimiter)
     prefix: Prefix = ts.option(factory=Prefix)
