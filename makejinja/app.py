a8da7fac03a08ba23ca9a7debc9c183fc7688ce6
Mirko Lenz <mirko@mirkolenz.com>
2023-02-06 11:33:19 +0100

    fix: modularize app, improve loader construction

--- makejinja/app.py
+++ makejinja/app.py
@@ -1,6 +1,7 @@
 import shutil
 import sys
 import typing as t
+from inspect import signature
 from pathlib import Path
 
 import yaml
@@ -18,43 +19,7 @@ except ModuleNotFoundError:
 
 from makejinja.config import Config
 
-
-def _from_yaml(path: Path) -> dict[str, t.Any]:
-    data = {}
-
-    with path.open("rb") as fp:
-        for doc in yaml.safe_load_all(fp):
-            data |= doc
-
-    return data
-
-
-def _from_toml(path: Path) -> dict[str, t.Any]:
-    with path.open("rb") as fp:
-        return tomllib.load(fp)
-
-
-DATA_LOADERS: dict[str, t.Callable[[Path], dict[str, t.Any]]] = {
-    ".yaml": _from_yaml,
-    ".yml": _from_yaml,
-    ".toml": _from_toml,
-}
-
-
-def _collect_files(paths: t.Iterable[Path], pattern: str = "**/*") -> t.List[Path]:
-    files = []
-
-    for path in paths:
-        if path.is_dir():
-            files.extend(
-                file
-                for file in sorted(path.glob(pattern))
-                if not file.name.startswith(".") and file.is_file()
-            )
-        elif path.is_file():
-            files.append(path)
-
-    return files
+__all__ = ["makejinja"]
 
 
 def makejinja(config: Config):
@@ -69,13 +34,29 @@ def makejinja(config: Config):
     for path in config.import_paths:
         sys.path.append(str(path.resolve()))
 
-    data: dict[str, t.Any] = {}
+    data = load_data(config)
+    env = init_jinja_env(config)
 
-    for path in _collect_files(config.data):
-        if loader := DATA_LOADERS.get(path.suffix):
-            data |= loader(path)
+    for loader in config.loaders:
+        process_loader(loader, env, data)
 
-    env = Environment(
+    if config.output.is_dir():
+        print(f"Remove '{config.output}' from previous run")
+        shutil.rmtree(config.output)
+
+    if config.copy_tree:
+        print(f"Copy file tree '{config.input}' -> '{config.output}'")
+        shutil.copytree(config.input, config.output)
+
+    config.output.mkdir(parents=True, exist_ok=True)
+
+    for input in config.input.glob(config.input_pattern):
+        if not input.is_dir():
+            render_file(input, config, env, data)
+
+
+def init_jinja_env(config: Config) -> Environment:
+    return Environment(
         loader=FileSystemLoader(config.input),
         extensions=config.extensions,
         block_start_string=config.delimiter.block_start,
@@ -100,71 +81,116 @@ def makejinja(config: Config):
         enable_async=config.internal.enable_async,
     )
 
-    loader_classes = [import_string(loader) for loader in config.loaders]
-    loaders: list[AbstractLoader] = []
 
-    for loader_class in loader_classes:
-        try:
-            loaders.append(loader_class(env, data))
-        except TypeError:
-            loaders.append(loader_class())
+def from_yaml(path: Path) -> dict[str, t.Any]:
+    data = {}
 
-    for loader in loaders:
-        if hasattr(loader, "extensions"):
-            load_extensions(env, loader.extensions())
+    with path.open("rb") as fp:
+        for doc in yaml.safe_load_all(fp):
+            data |= doc
 
-        if hasattr(loader, "globals"):
-            env.globals.update({func.__name__: func for func in loader.globals()})
+    return data
 
-        if hasattr(loader, "filters"):
-            env.filters.update({func.__name__: func for func in loader.filters()})
 
-        if hasattr(loader, "tests"):
-            env.tests.update({func.__name__: func for func in loader.tests()})
+def from_toml(path: Path) -> dict[str, t.Any]:
+    with path.open("rb") as fp:
+        return tomllib.load(fp)
 
-        if hasattr(loader, "policies"):
-            env.policies.update(loader.policies())
 
-        if hasattr(loader, "data"):
-            data.update(loader.data())
+DATA_LOADERS: dict[str, t.Callable[[Path], dict[str, t.Any]]] = {
+    ".yaml": from_yaml,
+    ".yml": from_yaml,
+    ".toml": from_toml,
+}
 
-    if config.output.is_dir():
-        print(f"Remove '{config.output}' from previous run")
-        shutil.rmtree(config.output)
 
-    if config.copy_tree:
-        print(f"Copy file tree '{config.input}' -> '{config.output}'")
-        shutil.copytree(config.input, config.output)
+def collect_files(paths: t.Iterable[Path], pattern: str = "**/*") -> t.List[Path]:
+    files = []
 
-    config.output.mkdir(parents=True, exist_ok=True)
+    for path in paths:
+        if path.is_dir():
+            files.extend(
+                file
+                for file in sorted(path.glob(pattern))
+                if not file.name.startswith(".") and file.is_file()
+            )
+        elif path.is_file():
+            files.append(path)
 
-    for input in config.input.glob(config.input_pattern):
-        if not input.is_dir():
-            relative_path = input.relative_to(config.input)
-            output = config.output / relative_path
-            output.parent.mkdir(parents=True, exist_ok=True)
+    return files
 
-            if relative_path.suffix == config.jinja_suffix:
-                template = env.get_template(str(relative_path))
 
-                # Remove the copied file if the tree has been duplicated
-                if config.copy_tree:
-                    output.unlink()
+def load_data(config: Config) -> dict[str, t.Any]:
+    data: dict[str, t.Any] = {}
 
-                if not config.keep_jinja_suffix:
-                    output = output.with_suffix("")
+    for path in collect_files(config.data):
+        if loader := DATA_LOADERS.get(path.suffix):
+            data |= loader(path)
 
-                rendered = template.render(data)
+    return data
 
-                # Write the rendered template if it has content
-                # Prevents empty macro definitions
-                if rendered.strip() == "" and not config.keep_empty:
-                    print(f"Skip '{input}'")
-                else:
-                    print(f"Render '{input}' -> '{output}'")
-                    with output.open("w") as f:
-                        f.write(rendered)
 
-            elif not config.copy_tree:
-                print(f"Copy '{input}' -> '{output}'")
-                shutil.copy2(input, output)
+def process_loader(
+    loader_name: str, env: Environment, data: t.MutableMapping[str, t.Any]
+):
+    cls: t.Type[AbstractLoader] = import_string(loader_name)
+    sig_params = signature(cls).parameters
+    params: dict[str, t.Any] = {}
+
+    if sig_params.get("env"):
+        params["env"] = env
+    if sig_params.get("environment"):
+        params["environment"] = env
+    if sig_params.get("data"):
+        params["data"] = data
+
+    loader = cls(**params)
+
+    if hasattr(loader, "extensions"):
+        load_extensions(env, loader.extensions())
+
+    if hasattr(loader, "globals"):
+        env.globals.update({func.__name__: func for func in loader.globals()})
+
+    if hasattr(loader, "filters"):
+        env.filters.update({func.__name__: func for func in loader.filters()})
+
+    if hasattr(loader, "tests"):
+        env.tests.update({func.__name__: func for func in loader.tests()})
+
+    if hasattr(loader, "policies"):
+        env.policies.update(loader.policies())
+
+    if hasattr(loader, "data"):
+        data.update(loader.data())
+
+
+def render_file(input: Path, config: Config, env: Environment, data: dict[str, t.Any]):
+    relative_path = input.relative_to(config.input)
+    output = config.output / relative_path
+    output.parent.mkdir(parents=True, exist_ok=True)
+
+    if relative_path.suffix == config.jinja_suffix:
+        template = env.get_template(str(relative_path))
+
+        # Remove the copied file if the tree has been duplicated
+        if config.copy_tree:
+            output.unlink()
+
+        if not config.keep_jinja_suffix:
+            output = output.with_suffix("")
+
+        rendered = template.render(data)
+
+        # Write the rendered template if it has content
+        # Prevents empty macro definitions
+        if rendered.strip() == "" and not config.keep_empty:
+            print(f"Skip '{input}'")
+        else:
+            print(f"Render '{input}' -> '{output}'")
+            with output.open("w") as f:
+                f.write(rendered)
+
+    elif not config.copy_tree:
+        print(f"Copy '{input}' -> '{output}'")
+        shutil.copy2(input, output)
