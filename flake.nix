0111a31547a4e677e9ac1e3d529ed28a8d3abc85
Mirko Lenz <mirko@mirkolenz.com>
2023-04-17 14:37:11 +0200

    build: add nix flake for development environment

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ python311 ];
+        };
+      }
+    );
+}
