0d196c808f5c49fadfa03640ecdcc82b91a0d523
Mirko Lenz <mirko@mirkolenz.com>
2023-04-30 14:51:07 +0200

    ci: switch to native devshell again

--- flake.nix
+++ flake.nix
@@ -2,16 +2,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    devshell.url = "github:numtide/devshell";
   };
-  outputs = inputs@{ nixpkgs, flake-parts, devshell, ... }:
+  outputs = inputs@{ nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ devshell.flakeModule ];
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem = { pkgs, ... }: {
-        devshells.default.devshell = {
+        devShells.default = pkgs.mkShell {
           packages = with pkgs; [ poetry python311 ];
-          startup.default.text = with pkgs; ''
+          shellHook = with pkgs; ''
             ${poetry}/bin/poetry env use ${python311}/bin/python
             ${poetry}/bin/poetry install --all-extras --no-root --no-interaction
           '';
