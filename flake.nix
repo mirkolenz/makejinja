10eba99c24d3687c85ee2ee70c537a80b53bd6de
Mirko Lenz <mirko@mirkolenz.com>
2023-04-30 22:00:20 +0200

    ci: use nix devenv for shell and docker

--- flake.nix
+++ flake.nix
@@ -2,17 +2,39 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+  outputs = inputs@{ nixpkgs, flake-parts, devenv, ... }:
+    let
+      pyproject = (builtins.fromTOML (builtins.readFile ./pyproject.toml));
+    in
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ devenv.flakeModule ];
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem = { pkgs, ... }: {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ poetry python311 ];
-          shellHook = with pkgs; ''
-            ${poetry}/bin/poetry env use ${python311}/bin/python
-            ${poetry}/bin/poetry install --all-extras --no-root --no-interaction
-          '';
+        devenv = {
+          processes.makejinja = {
+            exec = "python -m makejinja";
+          };
+          containers.processes = {
+            startupCommand = "--help";
+            version = pyproject.tool.poetry.version;
+            registry = "docker://ghcr.io/";
+          };
+          shells.default = {
+            languages.python = {
+              enable = true;
+              package = pkgs.python311;
+              poetry = {
+                enable = true;
+                activate.enable = true;
+                install.enable = true;
+              };
+            };
+          };
         };
       };
     };
