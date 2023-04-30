4efbb0bd96ddeccaa88530e2e05b049de5adead4
Mirko Lenz <mirko@mirkolenz.com>
2023-04-30 13:42:27 +0200

    build: use nix devshell

--- flake.nix
+++ flake.nix
@@ -2,19 +2,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
   };
-  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+  outputs = inputs@{ nixpkgs, flake-parts, devshell, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ devshell.flakeModule ];
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem = { pkgs, ... }: {
-        devShells.default =
-          pkgs.mkShell {
-            packages = with pkgs; [ poetry python311 ];
-            shellHook = with pkgs; ''
-              ${poetry}/bin/poetry env use ${python311}/bin/python
-              ${poetry}/bin/poetry install --all-extras --no-root --no-interaction
-            '';
-          };
+        devshells.default.devshell = {
+          packages = with pkgs; [ poetry python311 ];
+          startup.default.text = with pkgs; ''
+            ${poetry}/bin/poetry env use ${python311}/bin/python
+            ${poetry}/bin/poetry install --all-extras --no-root --no-interaction
+          '';
+        };
       };
     };
 }
