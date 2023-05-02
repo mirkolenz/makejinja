31eb0f71cfae7fce0dbf362b54d386c397f81def
Mirko Lenz <mirko@mirkolenz.com>
2023-05-02 10:08:17 +0200

    build: use real dockerfile again

--- flake.nix
+++ flake.nix
@@ -8,22 +8,12 @@
     };
   };
   outputs = inputs@{ nixpkgs, flake-parts, devenv, ... }:
-    let
-      pyproject = (builtins.fromTOML (builtins.readFile ./pyproject.toml));
-    in
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ devenv.flakeModule ];
       systems = nixpkgs.lib.systems.flakeExposed;
       perSystem = { pkgs, ... }: {
         devenv.shells.default = {
-          processes.makejinja = {
-            exec = "python -m makejinja";
-          };
-          containers.processes = {
-            startupCommand = "--help";
-            version = pyproject.tool.poetry.version;
-            registry = "docker://ghcr.io/";
-          };
+          name = "makejinja";
           languages.python = {
             enable = true;
             package = pkgs.python311;
