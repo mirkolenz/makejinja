2180cbad776fc4a3319ad611927fb46844ff1da6
Mirko Lenz <mirko@mirkolenz.com>
2023-05-04 20:40:02 +0200

    build: add poetry2nix to provide nix package

--- flake.nix
+++ flake.nix
@@ -6,17 +6,31 @@
       url = "github:cachix/devenv";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = inputs@{ nixpkgs, flake-parts, devenv, ... }:
+  outputs = inputs@{ nixpkgs, flake-parts, devenv, poetry2nix, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ devenv.flakeModule ];
       systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = { pkgs, ... }: {
+      perSystem = { pkgs, system, ... }:
+      let
+        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
+        py = pkgs.python311;
+      in
+      {
+        packages.default = mkPoetryApplication {
+          projectDir = ./.;
+          preferWheels = true;
+          python = py;
+        };
         devenv.shells.default = {
           name = "makejinja";
           languages.python = {
             enable = true;
-            package = pkgs.python311;
+            package = py;
             poetry = {
               enable = true;
               activate.enable = true;
