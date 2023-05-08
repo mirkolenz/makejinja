7b6ecb872cbad739e2cd085b4a082dcc2bf69e9f
Mirko Lenz <mirko@mirkolenz.com>
2023-05-08 10:33:13 +0200

    build: move docker image generation to nix

--- flake.nix
+++ flake.nix
@@ -10,34 +10,64 @@
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix2container = {
+      url = "github:nlewo/nix2container";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = inputs@{ nixpkgs, flake-parts, devenv, poetry2nix, ... }:
+  outputs = inputs@{ nixpkgs, flake-parts, devenv, poetry2nix, nix2container, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ devenv.flakeModule ];
       systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = { pkgs, system, ... }:
-      let
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
-        py = pkgs.python311;
-      in
-      {
-        packages.default = mkPoetryApplication {
-          projectDir = ./.;
-          preferWheels = true;
-          python = py;
-        };
-        devenv.shells.default = {
-          name = "makejinja";
-          languages.python = {
-            enable = true;
-            package = py;
-            poetry = {
+      perSystem = { pkgs, system, lib, ... }:
+        let
+          inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
+          inherit (nix2container.packages.${system}.nix2container) buildImage;
+          py = pkgs.python311;
+        in
+        {
+          packages = rec {
+            makejinja = mkPoetryApplication {
+              projectDir = ./.;
+              preferWheels = true;
+              python = py;
+            };
+            default = makejinja;
+            docker =
+              let
+                metaJson = builtins.getEnv "DOCKER_METADATA_OUTPUT_JSON";
+                meta = if metaJson == "" then { } else builtins.fromJSON metaJson;
+              in
+              buildImage {
+                inherit meta;
+                name = "makejinja";
+                tag = "latest";
+                config = {
+                  entrypoint = [ (lib.getExe makejinja) ];
+                  cmd = [ "--help" ];
+                };
+              };
+            # https://yuanwang.ca/posts/push-docker-image-to-gcr-with-nix.html
+            # docker = pkgs.dockerTools.buildImage {
+            #   name = "makejinja";
+            #   tag = "latest";
+            #   config = {
+            #     entrypoint = [ (lib.getExe makejinja) ];
+            #     cmd = [ "--help" ];
+            #   };
+            # };
+          };
+          devenv.shells.default = {
+            languages.python = {
               enable = true;
-              activate.enable = true;
-              install.enable = true;
+              package = py;
+              poetry = {
+                enable = true;
+                activate.enable = true;
+                install.enable = true;
+              };
             };
           };
         };
-      };
     };
 }
