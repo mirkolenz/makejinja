abe919dafae1fd309712ffc96ad645e83c831ea2
Mirko Lenz <mirko@mirkolenz.com>
2023-05-30 15:26:30 +0200

    build: update nix flake

--- flake.nix
+++ flake.nix
@@ -1,62 +1,67 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    systems.url = "github:nix-systems/default";
   };
-  outputs = inputs@{ nixpkgs, flake-parts, poetry2nix, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = { pkgs, system, lib, self', ... }:
-        let
-          inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication mkPoetryEnv;
-          poetryArgs = {
-            projectDir = ./.;
-            preferWheels = true;
-            python = pkgs.python311;
-          };
-        in
-        {
-          apps.copyDockerImage = {
-            type = "app";
-            program = builtins.toString (pkgs.writeShellScript "copyDockerImage" ''
-              IFS=$'\n' # iterate over newlines
-              set -x # echo on
-              for DOCKER_TAG in $DOCKER_METADATA_OUTPUT_TAGS; do
-                ${lib.getExe pkgs.skopeo} --insecure-policy copy "docker-archive:${self'.packages.dockerImage}" "docker://$DOCKER_TAG"
-              done
-            '');
-          };
-          packages =
-            let
-              app = mkPoetryApplication poetryArgs;
-            in
-            {
-              makejinja = app;
-              default = app;
-              dockerImage = pkgs.dockerTools.buildImage {
-                name = "makejinja";
-                tag = "latest";
-                created = "now";
-                config = {
-                  Entrypoint = [ (lib.getExe app) ];
-                  Cmd = [ "--help" ];
-                };
-              };
-            };
-          devShells.default =
-            let
-              venv = mkPoetryEnv poetryArgs;
-            in
-            pkgs.mkShell {
-              shellHook = ''
-                ln -sfn ${venv} .venv
-              '';
-              packages = [ venv pkgs.poetry ];
-            };
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    poetry2nix,
+    systems,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import systems;
+      perSystem = {
+        pkgs,
+        system,
+        lib,
+        self',
+        ...
+      }: let
+        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication mkPoetryEnv;
+        poetryArgs = {
+          projectDir = ./.;
+          preferWheels = true;
+          python = pkgs.python311;
         };
+        app = mkPoetryApplication poetryArgs;
+        venv = mkPoetryEnv poetryArgs;
+      in {
+        apps.copyDockerImage = {
+          type = "app";
+          program = builtins.toString (pkgs.writeShellScript "copyDockerImage" ''
+            IFS=$'\n' # iterate over newlines
+            set -x # echo on
+            for DOCKER_TAG in $DOCKER_METADATA_OUTPUT_TAGS; do
+              ${lib.getExe pkgs.skopeo} --insecure-policy copy "docker-archive:${self'.packages.dockerImage}" "docker://$DOCKER_TAG"
+            done
+          '');
+        };
+        packages = {
+          makejinja = app;
+          default = app;
+          dockerImage = pkgs.dockerTools.buildImage {
+            name = "makejinja";
+            tag = "latest";
+            created = "now";
+            config = {
+              Entrypoint = [(lib.getExe app)];
+              Cmd = ["--help"];
+            };
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            ln -sfn ${venv} .venv
+          '';
+          packages = [venv pkgs.poetry];
+        };
+      };
     };
 }
