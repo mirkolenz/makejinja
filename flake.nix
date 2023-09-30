e3c01389945c490d91029c7ac537f8b52734db09
Mirko Lenz <mirko@mirkolenz.com>
2023-09-30 20:37:01 +0200

    chore: switch to stable channel due to bug

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
     flocken = {
