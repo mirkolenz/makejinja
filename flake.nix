afbcc879975a8dc8f979ed2fe875c6c97a7a76fd
Mirko Lenz <mirko@mirkolenz.com>
2025-03-03 14:21:18 +0100

    build: use cachix

--- flake.nix
+++ flake.nix
@@ -27,6 +27,18 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://mirkolenz.cachix.org"
+      "https://pyproject-nix.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "mirkolenz.cachix.org-1:R0dgCJ93t33K/gncNbKgUdJzwgsYVXeExRsZNz5jpho="
+      "pyproject-nix.cachix.org-1:UNzugsOlQIu2iOz0VyZNBQm2JSrL/kwxeCcFGw+jMe0="
+    ];
+  };
   outputs =
     inputs@{
       self,
